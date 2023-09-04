0c0755b70875a9d583bb1c5e606729a22a850e44
Avinash Yadav <109527105+logan-14@users.noreply.github.com>
2023-09-04 12:39:10 +0530

    initial

--- VideoMethod.java
+++ VideoMethod.java
@@ -1,48 +1,66 @@
-import java.util.*;
-
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class VideoMethod {
-    List<Video> MovieList = new ArrayList<Video>();
-    public void addMovies(){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        boolean status = sc.nextBoolean();
-        double rat = sc.nextDouble();
-       
-        Video m = new Video(name, status, rat);
-        MovieList.add(m);
-        System.out.println("Initialized");
-    }
-    public void display(){
-        if(MovieList.isEmpty()){
-            System.out.println("No Movies");
+        List<Video> MovieList = new ArrayList<Video>();
+        public void addMovies()
+        {
+            Scanner in =new Scanner(System.in);
+            System.out.print("Enter the name of the movie:");
+            String mName=in.nextLine();
+            System.out.print("Enter the status of the movie(True/False):");
+            boolean status=in.nextBoolean();
+            System.out.print("Enter the ratings for the movie(0-5):");
+            double rating=in.nextDouble();
+            Video v=new Video(mName, status, rating);
+            MovieList.add(v);
+            System.out.println("Library Initialized");
         }
-        for(Video m : MovieList){
-            System.out.println("Movie : "+m.getName()+"  Status:"+m.getStatus()+"  Rating:"+m.getRating());
+        public void display()
+        {
+            if(MovieList.isEmpty())
+            {
+                System.out.println("No movies in the library");
+            }
+            for(Video m : MovieList)
+            {
+                System.out.println("Movie : " +m.getName()+"  "+"Status : "+m.getStatus()+"  "+"Rating "+m.getRating());
+            }
         }
-    }
-    public boolean rentOut(String name){
-        for(Video m : MovieList){
-            if(m.getName().equalsIgnoreCase(name)){
-                if(m.getStatus()){
-                    m.setStatus(false);
-                    return true;
+
+        boolean rentOut(String name)
+        {
+            for(Video m :MovieList)
+            {
+                if(m.getName().equalsIgnoreCase(name))
+                {
+                    if(m.getStatus())
+                    {
+                        m.setStatus(false);
+                        return true;
+                    }
                 }
+                
             }
             return false;
         }
-        return false;
-    }
-    public void collect(String name, double rat){
-        boolean flag = false;
-        for(Video m : MovieList){
-            if(m.getName().equalsIgnoreCase(name)){
-                m.setStatus(true);
-                flag= true;
-                Math.round(m.setRating((m.getRating()+rat)/2));
+
+        public void collect(String name,double rat)
+        {
+            boolean flag=false;
+            for(Video m :MovieList)
+            {
+                if(m.getName().equalsIgnoreCase(name))
+                {
+                    m.setStatus(true);
+                    flag=true;
+                    Math.round(m.setRating((m.getRating() + rat)/2));
+                }
+            }
+            if(!flag)
+            {
+                System.out.println("Requested Movie not rented out");
             }
         }
-        if(!flag){
-            System.out.println("Movie not rented out!");
-        }
     }
-}
+
